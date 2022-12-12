9770b8ea256b75576e5ae0ed5ae03e74f3f07371
tom.pietri <tom.pietri@agicap.com>
2022-12-12 18:31:16 +0100

    day12 brute force solution

--- src/main/kotlin/com/tompietri/aoc2022/utils/data/Point2D.kt
+++ src/main/kotlin/com/tompietri/aoc2022/utils/data/Point2D.kt
@@ -2,7 +2,7 @@ package com.tompietri.aoc2022.utils.data
 
 data class Point2D(val x: Int, val y: Int) : Comparable<Point2D> {
     override fun compareTo(other: Point2D): Int {
-        return if(this.y == other.y) {
+        return if (this.y == other.y) {
             if (this.x < other.x) {
                 -1
             } else {
@@ -14,4 +14,11 @@ data class Point2D(val x: Int, val y: Int) : Comparable<Point2D> {
             1
         }
     }
+
+    fun generateAdjacent(): List<Point2D> = listOf(
+        this.copy(x = this.x - 1),
+        this.copy(x = this.x + 1),
+        this.copy(y = this.y - 1),
+        this.copy(y = this.y + 1)
+    )
 }
