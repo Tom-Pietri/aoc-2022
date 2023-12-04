fc1549fadf31c681db3b88be0f54e49d369bf2b7
tom.pietri <tom.pietri@agicap.com>
2023-12-04 22:19:02 +0100

    day 3

--- src/main/kotlin/com/tompietri/aoc2022/day03/day.kt
+++ src/main/kotlin/com/tompietri/aoc2022/day03/day.kt
@@ -1,31 +1,135 @@
 package com.tompietri.aoc2022.day03
 
+import com.tompietri.aoc2022.utils.data.Point2D
+
 fun day3FirstSolution(input: List<String>): Int {
-    return input.map { Rucksack(it.take(it.length / 2), it.takeLast(it.length / 2)) }
-        .sumOf { it.score() }
+    val grid = mutableMapOf<Point2D, Cell>()
+    input.forEachIndexed { x, line ->
+        line.forEachIndexed { y, char ->
+            grid[Point2D(x, y)] = Cell(Point2D(x, y), char)
+        }
+    }
+
+    var result = 0
+    for (x in input.indices) {
+        var y = 0;
+        while (y < input[x].length) {
+            if (grid[Point2D(x, y)]!!.char.isDigit()) {
+                val digits = mutableListOf(grid[Point2D(x, y)]!!.char to y)
+                if (grid[Point2D(x, y + 1)]?.char?.isDigit() == true) {
+                    y++
+                    digits.add(grid[Point2D(x, y)]!!.char to y)
+                    if (grid[Point2D(x, y + 1)]?.char?.isDigit() == true) {
+                        y++
+                        digits.add(grid[Point2D(x, y)]!!.char to y)
+                    }
+                }
+                val adjacentCells =
+                    getAdjacentCells(Point2D(x, digits.first().second), Point2D(x, digits.last().second))
+                if (adjacentCells.any { grid[it]?.char != '.' && grid[it]?.char?.isDigit() == false }) {
+                    result += digits.map { it.first }.joinToString("").toInt()
+                }
+            }
+            y++
+        }
+    }
+
+    return result
 }
 
+fun getAdjacentCells(left: Point2D, right: Point2D): List<Point2D> {
+    val result = mutableListOf<Point2D>()
+    for (x in left.x - 1..right.x + 1) {
+        for (y in left.y - 1..right.y + 1) {
+            result.add(Point2D(x, y))
+        }
+    }
+    return result
+}
+
+fun getAdjacentCells(point: Point2D): List<Point2D> {
+    val result = mutableListOf<Point2D>()
+    for (x in point.x - 1..point.x + 1) {
+        for (y in point.y - 1..point.y + 1) {
+            if (x == point.x && y == point.y) continue
+            result.add(Point2D(x, y))
+        }
+    }
+    return result
+}
 
 fun day3SecondSolution(input: List<String>): Int {
-
-    return input.chunked(3)
-        .map { Group(it[0], it[1], it[2]) }
-        .sumOf { it.score() }
-}
-
-private data class Rucksack(val firstHalf: String, val secondHalf: String) {
-    fun score(): Int {
-        val secondHalfSet = secondHalf.toSet()
-        val char = firstHalf.first { secondHalfSet.contains(it) }
-        return if (char.isUpperCase()) char.code - 64 + 26 else char.code - 96
+    val grid = mutableMapOf<Point2D, Cell>()
+    input.forEachIndexed { x, line ->
+        line.forEachIndexed { y, char ->
+            grid[Point2D(x, y)] = Cell(Point2D(x, y), char)
+        }
     }
+
+    var result = 0
+    val gearAdjacentNumbers = mutableMapOf<Point2D, MutableList<Int>>()
+    for (x in input.indices) {
+        var y = 0;
+        while (y < input[x].length) {
+            if (grid[Point2D(x, y)]!!.char.isDigit()) {
+                val digits = mutableListOf(grid[Point2D(x, y)]!!.char to y)
+                if (grid[Point2D(x, y + 1)]?.char?.isDigit() == true) {
+                    y++
+                    digits.add(grid[Point2D(x, y)]!!.char to y)
+                    if (grid[Point2D(x, y + 1)]?.char?.isDigit() == true) {
+                        y++
+                        digits.add(grid[Point2D(x, y)]!!.char to y)
+                    }
+                }
+                val adjacentCellsWithGear =
+                    getAdjacentCells(Point2D(x, digits.first().second), Point2D(x, digits.last().second))
+                        .filter { grid[it]?.char == '*' }
+                val number = digits.map { it.first }.joinToString("").toInt()
+                adjacentCellsWithGear.forEach { gearPosition ->
+                    gearAdjacentNumbers.getOrPut(gearPosition) { mutableListOf() }.add(number)
+                }
+            }
+            y++
+        }
+    }
+
+    gearAdjacentNumbers.forEach { (gearPosition, adjacentNumbers) ->
+        if (adjacentNumbers.size == 2) {
+            result += adjacentNumbers.first() * adjacentNumbers.last()
+        }
+    }
+
+    return result
 }
 
-private data class Group(val first: String, val second: String, val third: String) {
-    fun score(): Int {
-        val secondSet = second.toSet()
-        val thirdSet = third.toSet()
-        val char = first.first { second.contains(it) && third.contains(it) }
-        return if (char.isUpperCase()) char.code - 64 + 26 else char.code - 96
+private fun parseLeftDigit(
+    grid: MutableMap<Point2D, Cell>,
+    x: Int,
+    y: Int
+): Int {
+    var digits = listOf(grid[Point2D(x, y)]!!.char)
+    if (grid[Point2D(x, y - 2)]?.char?.isDigit() == true) {
+        digits = listOf(grid[Point2D(x, y - 2)]!!.char) + digits
+        if (grid[Point2D(x, y - 3)]?.char?.isDigit() == true) {
+            digits = listOf(grid[Point2D(x, y - 3)]!!.char) + digits
+        }
     }
+    return digits.joinToString("").toInt()
 }
+
+private fun parseRightDigit(
+    grid: MutableMap<Point2D, Cell>,
+    x: Int,
+    y: Int
+): Int {
+    var digits = listOf(grid[Point2D(x, y)]!!.char)
+    if (grid[Point2D(x, y + 2)]?.char?.isDigit() == true) {
+        digits = listOf(grid[Point2D(x, y + 2)]!!.char) + digits
+        if (grid[Point2D(x, y + 3)]?.char?.isDigit() == true) {
+            digits = listOf(grid[Point2D(x, y + 3)]!!.char) + digits
+        }
+    }
+    return digits.joinToString("").toInt()
+}
+
+data class Cell(val position: Point2D, val char: Char)
--- src/test/kotlin/com/tompietri/aoc2022/day03/Day3test.kt
+++ src/test/kotlin/com/tompietri/aoc2022/day03/Day3test.kt
@@ -8,12 +8,12 @@ class Day3test {
     @Test
     fun `First solution - My input should return 68775`() {
         val input = readInputWithMultipleLines(2022, 3)
-        assertThat(day3FirstSolution(input)).isEqualTo(7889)
+        assertThat(day3FirstSolution(input)).isEqualTo(521601)
     }
 
     @Test
     fun `Second solution - My input should return 1215`() {
         val input = readInputWithMultipleLines(2022, 3)
-        assertThat(day3SecondSolution(input)).isEqualTo(2825)
+        assertThat(day3SecondSolution(input)).isEqualTo(467835)
     }
 }
